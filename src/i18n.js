c216947ea797f872e31ff69c782fa2436f6dfafe
Francesco Formisano <keccofor@gmail.com>
2023-09-23 11:35:55 +0200

    fixed i18n

--- src/i18n.js
+++ src/i18n.js
@@ -2,8 +2,32 @@ import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
+import translationEN from "./locales/en/translation.json";
+import translationIT from "./locales/it/translation.json";
 
-i18next.use(initReactI18next).use(LanguageDetector).use(Backend).init({
-  debug: true,
-  fallbackLng: "it",
-});
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  it: {
+    translation: translationIT,
+  },
+};
+
+i18next
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(Backend)
+  .init({
+    debug: true,
+    fallbackLng: "it",
+  })
+  .init({
+    debug: true,
+    fallbackLng: "en",
+    lng: "it",
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+    resources,
+  });

f33a30d6086af730565b8a365aaed8e4a1edbd07
Francesco Formisano <keccofor@gmail.com>
2023-09-25 00:08:35 +0200

    animated scroll to top button

--- src/components/GoUp.jsx
+++ src/components/GoUp.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import { AiOutlineArrowUp } from "react-icons/ai";
+import { useState, useEffect } from "react";
 
 const GoUp = () => {
+  const [offset, setOffset] = useState(false);
+  useEffect(() => {
+    window.onscroll = () => {
+      setOffset(window.scrollY);
+    };
+  }, []);
+  const opacity1 = {
+    opacity: 1,
+  };
+  const opacity0 = {
+    opacity: 0,
+  };
   return (
     <>
-      <div className="cursor-pointer border-c-blue bg-c-blue rounded-full text-c-inside fixed bottom-5 right-5 w-[50px] h-[50px] text-3xl flex justify-center transition duration-500">
-        <a href="#" className="m-auto">
+      <div
+        className="cursor-pointer border-c-blue bg-c-blue rounded-full text-c-inside fixed bottom-5 right-5 w-[50px] h-[50px] text-3xl flex justify-center transition duration-500"
+        style={offset > 100 ? opacity1 : opacity0}
+      >
+        <button
+          className="m-auto"
+          onClick={() => {
+            window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+          }}
+        >
           <AiOutlineArrowUp></AiOutlineArrowUp>
-        </a>
+        </button>
       </div>
     </>
   );
